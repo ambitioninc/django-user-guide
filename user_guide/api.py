9f7145bcfec7de6acdacdbe27b3ee55d46aa303a
Jeffery McRiffey <jeff.mcriffey@tryambition.com>
2014-03-17 17:04:55 -0400

    api tests

--- user_guide/api.py
+++ user_guide/api.py
@@ -1,17 +1,59 @@
-from user_guide.modles import Guide
+from django.contrib.auth.models import User
+from tastypie import fields
 from tastypie.authentication import SessionAuthentication
 from tastypie.authorization import Authorization
-from tastypie.resources import NamespacedModelResource, ALL
+from tastypie.resources import NamespacedModelResource, ALL, ALL_WITH_RELATIONS
+from user_guide.modles import Guide, GuideInfo
+
+
+class GuideUserResource(NamespacedModelResource):
+    class Meta:
+        urlconf_namespace = 'user_guide'
+        queryset = User.objects.all()
+        authorization = Authorization()
+        authentication = SessionAuthentication()
+        fields = ['email', 'date_joined']
+        filtering = {'id': ['exact']}
 
 
 class GuideResource(NamespacedModelResource):
     class Meta:
-        urlconf_namespace = 'account'
+        urlconf_namespace = 'user_guide'
         queryset = Guide.objects.all()
         authorization = Authorization()
         authentication = SessionAuthentication()
         filtering = {
             'id': ['exact'],
             'name': ['exact'],
-            'creation_time': ALL
+            'creation_time': ALL_WITH_RELATIONS
+        },
+        ordering = {
+            'id': ['exact'],
+            'name': ['exact'],
+            'creation_time': ALL_WITH_RELATIONS
+        }
+
+
+class GuideInfoResource(NamespacedModelResource):
+    user = fields.ForeignKey(GuideUserResource, 'guide_user', full=True)
+    guide = fields.ForeignKey(GuideResource, 'guide', full=True)
+
+    class Meta:
+        urlconf_namespace = 'user_guide'
+        queryset = GuideInfo.objects.all()
+        authorization = Authorization()
+        authentication = SessionAuthentication()
+        filtering = {
+            'id': ['exact'],
+            'name': ['exact'],
+            'finished': ALL,
+            'finished_time': ALL_WITH_RELATIONS,
+            'guide': ALL_WITH_RELATIONS,
+            'user': ALL
+        },
+        ordering = {
+            'id': ['exact'],
+            'name': ['exact'],
+            'finished': ALL,
+            'finished_time': ALL_WITH_RELATIONS
         }
---
+++ user_guide/tests/api_test.py
@@ -0,0 +1,31 @@
+import json
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from freezegun import freeze_time
+from user_guide.models import Guide, GuideInfo
+
+
+@freeze_time('2014-03-17 00:00:00')
+class GuideInfoResourceTest(TestCase):
+    def test_guide_info_resource(self):
+        user = User.objects.create(username='test@test.com', email='test@test.com', password='test')
+        guide = Guide.objects.create(
+            html='<div>Hello Test!</div>',
+            view_class_name='TestView',
+            guide_name='Test Guide',
+            guide_type='Window'
+        )
+        GuideInfo.objects.create(
+            user=user,
+            guide=guide
+        )
+
+        self.api_client.client.login(username='test@test.com', password='test', format='json', data={
+            'guide__view_class_name': 'TestView',
+            'user': user.id
+        })
+        resp = self.api_client.get('/user-guide/guideinfo')
+        guides = json.loads(resp)['objects']
+
+        self.assertEqual(len(guides), 1)
