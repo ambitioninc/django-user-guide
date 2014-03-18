8d0a91f909a9d98e41c4f74f12a9abe7647571d3
vagrant <vagrant@precise64.(none)>
2014-03-18 19:24:54 +0000

    updated api tests

--- test_project/wsgi.py
+++ test_project/wsgi.py
@@ -1,5 +1,5 @@
 import os
-from os.path import dirname
+
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", ".settings")
--- user_guide/api.py
+++ user_guide/api.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
-from tastypie import fields
+from tastypie import fields, http
 from tastypie.authentication import SessionAuthentication
 from tastypie.authorization import Authorization
+from tastypie.exceptions import ImmediateHttpResponse
 from tastypie.resources import NamespacedModelResource, ALL, ALL_WITH_RELATIONS
 from user_guide.models import Guide, GuideInfo
 
@@ -13,7 +16,8 @@ class GuideUserResource(NamespacedModelResource):
         queryset = User.objects.all()
         authorization = Authorization()
         authentication = SessionAuthentication()
-        list_allowed_methods = []
+        include_resource_uri=True
+        list_allowed_methods = ['get']
         fields = ['email', 'date_joined']
         filtering = {
             'id': ['exact'],
@@ -32,6 +36,7 @@ class GuideResource(NamespacedModelResource):
         queryset = Guide.objects.all()
         authorization = Authorization()
         authentication = SessionAuthentication()
+        include_resource_uri=True
         filtering = {
             'id': ['exact'],
             'view_class_name': ALL,
@@ -53,17 +58,48 @@ class GuideInfoResource(NamespacedModelResource):
         queryset = GuideInfo.objects.all()
         authorization = Authorization()
         authentication = SessionAuthentication()
+        include_resource_uri=True
+        list_allowed_methods = ['get', 'put']
+        fields_allowed_update = ['finished']
         filtering = {
             'id': ['exact'],
             'finished': ALL,
             'finished_time': ALL_WITH_RELATIONS,
             'guide': ALL_WITH_RELATIONS,
-            'user': ALL
+            'user': ALL_WITH_RELATIONS
         }
         ordering = {
             'id': ['exact'],
             'finished': ALL,
             'finished_time': ALL_WITH_RELATIONS,
             'guide': ALL_WITH_RELATIONS,
-            'user': ALL
+            'user': ALL_WITH_RELATIONS
         }
+
+    def alter_deserialized_detail_data(self, request, deserialized):
+        """
+        Enforces that the request conforms to PUT guidelines.
+            - A user can only update the fields specified in fields_allowed_update.
+        """
+
+        if set(deserialized.keys()) - set(self._meta.fields_allowed_update):
+            raise ImmediateHttpResponse(
+                response=http.HttpBadRequest('Can only update fields: {0}'.format(
+                    ', '.join(self._meta.fields_allowed_update)
+                ))
+            )
+        return super(GuideInfoResource, self).alter_deserialized_detail_data(request, deserialized)
+
+    def put_detail(self, request, **kwargs):
+        """
+        Enforces that the request conforms to PUT guidelines.
+            - A user can only update the fields specified in fields_allowed_update.
+        """
+        guide_info = GuideInfo.objects.get(pk=kwargs.get('pk'))
+
+        if guide_info.user.id != request.user.id:
+            raise ImmediateHttpResponse(
+                response=http.HttpBadRequest('User must match request\'s user.')
+            )
+
+        super(GuideInfoResource, self).put_detail(request, **kwargs)
