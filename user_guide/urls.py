17f319899872d98f61c55534ddf79a4332edabd5
Jeffery McRiffey <jeff.mcriffey@tryambition.com>
2014-03-20 11:54:27 -0400

    lint

--- user_guide/urls.py
+++ user_guide/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import NamespacedApi
+
 from user_guide.api import GuideResource, GuideInfoResource
 
-user_guide_api = NamespacedApi(api_name='api', urlconf_namespace='user_guide')
 
+user_guide_api = NamespacedApi(api_name='api', urlconf_namespace='user_guide')
 user_guide_api.register(GuideResource())
 user_guide_api.register(GuideInfoResource())
 
