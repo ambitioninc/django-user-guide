e63a54ca789744a6f5b4c51265f838f37ef251ae
Travis Truett <travis.truett@gmail.com>
2014-04-05 12:01:28 -0400

    Added human-readable name to guide field

--- user_guide/admin.py
+++ user_guide/admin.py
@@ -9,7 +9,10 @@ if 'django.contrib.admin' in settings.INSTALLED_APPS:
         list_display = ('guide_name', 'guide_tag', 'guide_importance', 'creation_time')
 
     class GuideInfoAdmin(admin.ModelAdmin):
-        list_display = ('user', 'guide', 'is_finished', 'finished_time')
+        list_display = ('user', 'guide_name', 'is_finished', 'finished_time')
+
+        def guide_name(self, obj):
+            return obj.guide.guide_name
 
     admin.site.register(Guide, GuideAdmin)
     admin.site.register(GuideInfo, GuideInfoAdmin)
