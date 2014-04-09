5a83bfd1b195e70a3d0100ec91a54544443c1af2
Travis Truett <travis.truett@gmail.com>
2014-04-09 17:41:36 -0400

    removed print statement

--- user_guide/tests/model_tests.py
+++ user_guide/tests/model_tests.py
@@ -7,6 +7,5 @@ from user_guide.models import Guide
 class GuideTest(TestCase):
 
     def test_guide_unicode(self):
-        print "im an idiot"
         guide_obj = G(Guide, guide_name='test_name')
         self.assertEqual(unicode(guide_obj), 'test_name')
